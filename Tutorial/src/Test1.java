ebdede8eeddec25b02e80d35a533b97b8b001fb5
Poonamchand Sahu <poonamchandsahu.ssgi@gmail.com>
2019-07-16 17:29:34 +0530

    16/07/2019

---
+++ Tutorial/src/Test1.java
@@ -0,0 +1,12 @@
+public class Test1 {
+
+	public static void main(String[] args) {
+		int[] nums = { 1, 2, 3, 4, 5 };
+		int result = 0;
+		for (int i : nums) {
+			result += i;
+		}
+		System.out.println(result);
+	}
+
+}
